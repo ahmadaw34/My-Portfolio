7fc3fc06947b71e34b7e7ca89ce9866be6f5ed08
ahmadaw <ahmadaw@post.bgu.ac.il>
2025-08-05 14:03:01 +0300

    change profile picture

--- README.md
+++ README.md
@@ -148,7 +148,7 @@ Eg:
 ```javascript
 export const personalData = {
   name: "AHMAD AWAWDY",
-  profile: '/profile.png',
+  profile: '/profile.jpg',
   designation: "Software Engineer",
   description: "My name is AHMAD AWAWDY. Aspiring Software Engineer seeking a new position to build impactful solutions. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving.",
   email: 'awawdy.ahmad@gmail.com',
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -1,6 +1,6 @@
 export const personalData = {
   name: "AHMAD AWAWDY",
-  profile: '/profile.png',
+  profile: '/profile.jpg',
   designation: "Software Engineer",
   description: "My name is Ahmad Awawdy, an aspiring Software Engineer passionate about building impactful solutions. I am a fast learner with a strong self-learning mindset and a deep curiosity for new technologies. I thrive on solving complex problems and continuously seek opportunities to grow and contribute through meaningful work.",
   email: 'awawdy.ahmad@gmail.com',
